a0c3129376d4d97db282030109fb768cc6846ebe
Pringgo Juni S <odyinggo@gmail.com>
2024-08-16 09:50:04 +0700

    fix

--- app/Rules/UniqueStaffPositionInVillage.php
+++ app/Rules/UniqueStaffPositionInVillage.php
@@ -22,22 +22,24 @@ class UniqueStaffPositionInVillage implements ValidationRule
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         /* jika jabatan selain sekretaris dan kepala, maka bebaskan */
-        // $positions = [
-        //     key_option('sekretaris_desa'),
-        //     key_option('kepala_desa'),
-        // ];
+        $positions = [
+            key_option('sekretaris_desa'),
+            key_option('kepala_desa'),
+        ];
 
-        // if (in_array($this->staff_position_id, $positions)) {
-            $query = VillageStaff::where('village_id', $this->village_id)
-                                 ->where('position_type_id', $this->staff_position_id);
+        if (!in_array($this->staff_position_id, $positions)) {
+            return;
+        }
     
-            if ($this->ignore_id) {
-                $query->where('id', '!=', $this->ignore_id);
-            }
-    
-            if ($query->exists()) {
-                $fail('The selected staff position is already taken in this village.');
-            }
-        // }
+        $query = VillageStaff::where('village_id', $this->village_id)
+                                ->where('position_type_id', $this->staff_position_id);
+
+        if ($this->ignore_id) {
+            $query->where('id', '!=', $this->ignore_id);
+        }
+
+        if ($query->exists()) {
+            $fail('The selected staff position is already taken in this village.');
+        }
     }
 }
