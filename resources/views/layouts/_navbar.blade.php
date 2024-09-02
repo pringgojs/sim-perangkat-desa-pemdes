f8f766bbb6ec0e2199abfaef9d6687a945ed108d
Pringgo Juni S <odyinggo@gmail.com>
2024-09-02 15:17:39 +0700

    fix bug dropdown navbar

--- resources/views/layouts/_navbar.blade.php
+++ resources/views/layouts/_navbar.blade.php
@@ -82,8 +82,8 @@
                         aria-current="page">Dashboard</a>
                 </li>
                 @if (auth()->user()->hasRole('administrator'))
-                    <li x-data="dropdown()">
-                        <button @click="toggle()" id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
+                    <li x-data="dropdown()" @click.outside="close()">
+                        <button @click="toggle()" id="dropdownNavbar1Link" data-dropdown-toggle="dropdownNavbar1"
                             class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Master
                             <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                 fill="none" viewBox="0 0 10 6">
@@ -92,8 +92,8 @@
                             </svg>
                         </button>
                         <!-- Dropdown menu -->
-                        <div id="dropdownNavbar" x-show="open" @click.outside="close()"
-                            class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                        <div x-show="open" @click.outside="close()" id="dropdownNavbar1"
+                            class="z-10 absolute mt-5 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                             <ul class="py-2 text-sm text-gray-700 dark:text-gray-400"
                                 aria-labelledby="dropdownLargeButton">
                                 <li>
@@ -120,9 +120,9 @@
                         </svg>
                     </button>
                     <!-- Dropdown menu -->
-                    <div id="dropdownDBLink" x-show="open" @click.outside="close()"
+                    <div id="dropdownDBLink" x-show="open"
                         class="z-10 font-normal absolute mt-5 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-400"
+                        <ul @click.outside="close()" class="py-2 text-sm text-gray-700 dark:text-gray-400"
                             aria-labelledby="dropdownLargeButton">
                             @foreach (\App\Models\Option::positionTypes()->get() as $item)
                                 <li wire:key="option-staff-{{ $item->id }}">
@@ -161,6 +161,7 @@
                 this.open = !this.open;
             },
             close() {
+                console.log('kepanggil gak')
                 this.open = false;
             },
         }));
