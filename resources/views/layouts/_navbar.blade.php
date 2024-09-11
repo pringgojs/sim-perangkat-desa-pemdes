8e53c3abe51cb4cdaead1c9ea761b8de52027d24
Pringgo Juni S <odyinggo@gmail.com>
2024-09-11 08:41:49 +0700

    hide navbar user

--- resources/views/layouts/_navbar.blade.php
+++ resources/views/layouts/_navbar.blade.php
@@ -165,11 +165,11 @@
                             </ul>
                         </div>
                     </li>
-                    <li>
+                    {{-- <li>
                         <a href="{{ route('user.index') }}" wire:navigate
                             class="block py-2 pl-3 pr-4 @if (request()->segment(1) == 'user') text-blue-700 @else text-gray-900 @endif rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                             User</a>
-                    </li>
+                    </li> --}}
                 @endif
                 @if (auth()->user()->hasRole('operator'))
                     <li>
