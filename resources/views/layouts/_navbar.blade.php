658fcc3fc5dfd883628e80348358e3f8c0853fb0
Pringgo Juni S <odyinggo@gmail.com>
2024-09-17 09:02:46 +0700

    update navbar menu

--- resources/views/layouts/_navbar.blade.php
+++ resources/views/layouts/_navbar.blade.php
@@ -160,7 +160,8 @@
                                 </li>
                                 <li>
                                     <a href="{{ route('statistic') }}" wire:navigate
-                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Statistik</a>
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Statistik
+                                        PD Akan Pensiun</a>
                                 </li>
                                 <li>
                                     <a href="{{ route('statistic-status-data') }}" wire:navigate
