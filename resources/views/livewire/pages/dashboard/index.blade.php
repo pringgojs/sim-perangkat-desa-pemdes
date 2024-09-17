0ba7b400f6539c247c3fb2f455f7ca4e30ff301f
Pringgo Juni S <odyinggo@gmail.com>
2024-09-17 09:34:33 +0700

    enable stats

--- resources/views/livewire/pages/dashboard/index.blade.php
+++ resources/views/livewire/pages/dashboard/index.blade.php
@@ -2,7 +2,7 @@
 
     @livewire('pages.dashboard.stats')
     {{-- daftar menunggu persetujuan --}}
-    @if (auth()->user()->hasRole('administrator'))
-        @livewire('pages.dashboard.section.statStatus')
-    @endif
+    {{-- @if (auth()->user()->hasRole('administrator')) --}}
+    @livewire('pages.dashboard.section.statStatus')
+    {{-- @endif --}}
 </div>
