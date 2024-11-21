56969deab3722451fb3f0636be1fdf923c7fbcee
Pringgo Juni S <odyinggo@gmail.com>
2024-11-21 14:34:52 +0700

    add table skeleton

---
+++ app/Livewire/Utils/SkeletonTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Utils;
+
+use Livewire\Component;
+
+class SkeletonTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.utils.skeleton-table');
+    }
+}
---
+++ resources/views/livewire/utils/skeleton-table.blade.php
@@ -0,0 +1,53 @@
+<div class="space-y-4">
+    <!-- Skeleton Table Title -->
+    <div class="h-6 bg-gray-200 rounded animate-pulse w-1/4 "></div>
+
+    <!-- Skeleton Table -->
+    <div class="overflow-x-auto">
+        <div class="w-full border border-gray-300 shadow rounded-lg bg-white">
+            <!-- Skeleton Header -->
+            <div class="h-12 bg-gray-100 animate-pulse flex items-center px-4">
+                <div class="w-1/12 h-10 bg-gray-300 rounded mr-2"></div>
+                <div class="w-6/12 h-10 bg-gray-300 rounded mr-2"></div>
+                <div class="w-2/12 h-10 bg-gray-300 rounded mr-2"></div>
+                <div class="w-2/12 h-10 bg-gray-300 rounded mr-2"></div>
+                <div class="w-1/12 h-10 bg-gray-300 rounded"></div>
+            </div>
+            <!-- Skeleton Rows -->
+            <div class="divide-y divide-gray-200">
+                <div class="h-10 bg-gray-50 animate-pulse flex items-center px-4">
+                    <div class="w-1/12 h-4 bg-gray-300 rounded mr-2"></div>
+                    <div class="w-6/12 h-4 bg-gray-300 rounded mr-2"></div>
+                    <div class="w-2/12 h-4 bg-gray-300 rounded mr-2"></div>
+                    <div class="w-2/12 h-4 bg-gray-300 rounded mr-2"></div>
+                    <div class="w-1/12 h-4 bg-gray-300 rounded"></div>
+                </div>
+                <div class="h-10 bg-gray-50 animate-pulse flex items-center px-4">
+                    <div class="w-1/12 h-4 bg-gray-300 rounded mr-2"></div>
+                    <div class="w-6/12 h-4 bg-gray-300 rounded mr-2"></div>
+                    <div class="w-2/12 h-4 bg-gray-300 rounded mr-2"></div>
+                    <div class="w-2/12 h-4 bg-gray-300 rounded mr-2"></div>
+                    <div class="w-1/12 h-4 bg-gray-300 rounded"></div>
+                </div>
+                <div class="h-10 bg-gray-50 animate-pulse flex items-center px-4">
+                    <div class="w-1/12 h-4 bg-gray-300 rounded mr-2"></div>
+                    <div class="w-6/12 h-4 bg-gray-300 rounded mr-2"></div>
+                    <div class="w-2/12 h-4 bg-gray-300 rounded mr-2"></div>
+                    <div class="w-2/12 h-4 bg-gray-300 rounded mr-2"></div>
+                    <div class="w-1/12 h-4 bg-gray-300 rounded"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Skeleton Pagination -->
+    <div class="flex justify-between items-center mt-4">
+        <div class="h-4 bg-gray-300 animate-pulse rounded w-24"></div>
+        <div class="flex space-x-2">
+            <div class="h-8 w-8 bg-gray-300 rounded-full animate-pulse"></div>
+            <div class="h-8 w-8 bg-gray-300 rounded-full animate-pulse"></div>
+            <div class="h-8 w-8 bg-gray-300 rounded-full animate-pulse"></div>
+        </div>
+        <div class="h-4 bg-gray-300 animate-pulse rounded w-24"></div>
+    </div>
+</div>
