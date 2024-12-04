e464bfc867b25a2c2d33e6d7e312285f7f9de6ee
Pringgo Juni S <odyinggo@gmail.com>
2024-12-04 15:27:39 +0700

    add badge notification pending

--- resources/views/layouts/_navbar.blade.php
+++ resources/views/layouts/_navbar.blade.php
@@ -28,6 +28,25 @@
                         {{ $notif }}</div>
                 </a>
             @endif
+
+            @php
+                $notif = \App\Models\VillageStaff::pending()->count();
+            @endphp
+            @if ($notif)
+                <a href="{{ route('village-staff.index', ['statusData' => key_option('diajukan')]) }}"
+                    class="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
+                    <span class="absolute -inset-1.5"></span>
+                    <span class="sr-only">View notifications</span>
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                    </svg>
+                    <div
+                        class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">
+                        {{ $notif }}</div>
+                </a>
+            @endif
             {{-- <template> --}}
             {{-- profile --}}
             <button type="button" data-dropdown-toggle="profile-user-dropdown"
