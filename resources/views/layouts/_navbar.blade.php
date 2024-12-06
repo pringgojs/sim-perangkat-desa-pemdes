d9967c468b0d932c37d7e2033a44fd0ce5a92e6e
Pringgo Juni S <odyinggo@gmail.com>
2024-12-06 12:39:10 +0700

    add number

--- resources/views/layouts/_navbar.blade.php
+++ resources/views/layouts/_navbar.blade.php
@@ -237,10 +237,11 @@
                             class="z-10 hidden mt-5 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                             <ul class="py-2 text-sm text-gray-700 dark:text-gray-400"
                                 aria-labelledby="dropdownLargeButton">
-                                @foreach (docs() as $item)
+                                @foreach (docs() as $i => $item)
                                     <li>
                                         <a href="{{ $item['link'] }}" target="_blank"
-                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ $item['label'] }}</a>
+                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ ++$i }}.
+                                            {{ $item['label'] }}</a>
                                     </li>
                                 @endforeach
                             </ul>
